8581a430ee64de0f041797f334141e1f2be1ab34
Luiz Imbroisi <luizimbroisi@gmail.com>
2025-09-03 08:40:06 -0300

    code crean up

--- src/PocCa3/config.ts
+++ src/PocCa3/config.ts
@@ -1,13 +1,13 @@
 export const CELL_HEIGHT_PX = 28;
 export const CELL_BORDER_COLOR = '#ddd';
 
+// TODO: maybe replace by selectable number of years (e.g. selected by the user)
+export const NUMBER_OF_YEARS = 3;
 export const YEAR_CELL_WIDTH_PX = 400;
-export const TOTAL_DAYS_AFTER_TODAY = 30;
+
+// TODO: review this methode when definition on how the pagination will be implemented (because of collase/expand sections)
 export const ROWS_BY_PAGE = 20;
 
 export const TODAY_LINE_COLOR = '#45acc8';
 
 export const LINKS_COLORS = ['green', 'pink'];
-
-// TODO: maybe replace by selectable number of years (e.g. selected by the user)
-export const NUMBER_OF_YEARS = 3;
