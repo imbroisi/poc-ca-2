a2aff049db500aad49a4320b1104fb9e3a2361fe
Ana Ventura <anaventura1811@gmail.com>
2025-09-11 14:16:05 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
-import { ModalProvider } from './HoldingsHistory/context/ModalContext';
 import HistoryLink from './HoldingsHistory/HoldingsHistory';
 
 function App() {
   return (
     <div style={{ margin: '100px', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <ModalProvider>
         <HistoryLink />
-      </ModalProvider>
     </div>
   );
 }
