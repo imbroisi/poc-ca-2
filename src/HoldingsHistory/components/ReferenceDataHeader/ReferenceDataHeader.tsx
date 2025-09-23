3db530879b73368446ee19bc4038d047e397936e
Ana Ventura <anaventura1811@gmail.com>
2025-09-23 12:35:11 -0300

    clean up code, refactor, css adjustments

--- src/HoldingsHistory/components/ReferenceDataHeader/ReferenceDataHeader.tsx
+++ src/HoldingsHistory/components/ReferenceDataHeader/ReferenceDataHeader.tsx
@@ -17,28 +17,11 @@ const ReferenceDataHeader = ({ columnHeadings }: ReferenceDataHeaderProps) => {
     <>
       <div className="reference-data-header-container" style={{ borderBottom: `1px solid ${MAIN_BORDER_COLOR}`}}
       />
-          {/* Header row placeholder to align with date header */}
-          <div style={{ 
-            height: '40px',
-            backgroundColor: 'white',
-            position: 'sticky',
-            top: '30px',
-            zIndex: 98,
-            borderBottom: `1px solid ${MAIN_BORDER_COLOR}`,
-            boxSizing: 'border-box',
-            width: '100%',
-            maxWidth: 300,
-            background: 'white',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'flex-end',
-            fontSize: 12,
-            fontWeight: 600,
-            justifyContent: 'space-between' }}>
-            
+          {/* Header row */}
+          <div className="reference-data-row" style={{ borderBottom: `1px solid ${MAIN_BORDER_COLOR}`}}>
             {columnHeadings?.map(({ heading, enabled }, index) => (
               <ColumnHeading
-                key={`${heading}-${index}`}
+                key={`${heading}-$${index}`}
                 heading={heading}
                 enabled={enabled}
                 isLast={index ===  columnHeadings?.length - 1}
